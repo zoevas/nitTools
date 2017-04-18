ad68d0bb1fd39f49b0441a3d9341c2b6c07e9741
zoevas <zoevas1@gmail.com>
2017-04-18 18:16:09 +0300

    Update SchedulerChooserFragment.java

--- src/com/nitos/testbed/tools/SchedulerChooserFragment.java
+++ src/com/nitos/testbed/tools/SchedulerChooserFragment.java
@@ -438,132 +438,119 @@ public  class SchedulerChooserFragment extends Fragment   implements OnClickList
 				for (int lease_i = 0; lease_i < resources.length(); lease_i++) {
 					JSONObject leaseObj = resources.getJSONObject(lease_i);
 							    
-//Ignore the leases with status = past and status = cancelled
-String leaseStatus = leaseObj.getString("status");
-							    if(leaseStatus.equals("past") || leaseStatus.equals("cancelled"))
-							    	continue;
+					//Ignore the leases with status = past and status = cancelled
+					String leaseStatus = leaseObj.getString("status");
+					if(leaseStatus.equals("past") || leaseStatus.equals("cancelled"))
+						continue;
 								
-								//set the reservation(date and time, from and until)  in each node of HashMap
-								Reservation reservation = setTimeAndDateOnReservation(leaseObj);
-							    //Get all the components in each lease
-								JSONArray componentsArray = leaseObj.getJSONArray(Constants.TAG_COMPONENTS);
+					//set the reservation(date and time, from and until)  in each node of HashMap
+					Reservation reservation = setTimeAndDateOnReservation(leaseObj);
+					//Get all the components in each lease
+					JSONArray componentsArray = leaseObj.getJSONArray(Constants.TAG_COMPONENTS);
 								
-								ArrayList<String> check_for_resources_names_duplicates = new ArrayList<String>();
-							    //Looping through all components and add the reservation to the ArrayList of the appropriate node of the HashMap
-								for(int component_i = 0; component_i < componentsArray.length(); component_i++)	{
-									JSONObject componentObj = componentsArray.getJSONObject(component_i).getJSONObject(Constants.TAG_COMPONENT);
-									Log.i("component name ", componentObj.getString("name"));
+					ArrayList<String> check_for_resources_names_duplicates = new ArrayList<String>();
+					//Looping through all components and add the reservation to the ArrayList of the appropriate node of the HashMap
+					for(int component_i = 0; component_i < componentsArray.length(); component_i++)	{
+						JSONObject componentObj = componentsArray.getJSONObject(component_i).getJSONObject(Constants.TAG_COMPONENT);
+						Log.i("component name ", componentObj.getString("name"));
 									
-									String resource_name = componentObj.getString("name");
+						String resource_name = componentObj.getString("name");
 									
-									//A node can be contained two times in the components array
-									if(check_for_resources_names_duplicates.contains(resource_name)){
-								    	//NA PSAKSW GIA THROWS EXCEPTION
-								    	Log.i("duplicate Leases","duplicate");
-								    	continue;
-								    }
-								    else{
-								    	check_for_resources_names_duplicates.add(resource_name);
-								    }
+						//A node can be contained two times in the components array
+						if(check_for_resources_names_duplicates.contains(resource_name)){
+							//NA PSAKSW GIA THROWS EXCEPTION
+							Log.i("duplicate Leases","duplicate");
+							continue;
+						} else {
+							check_for_resources_names_duplicates.add(resource_name);
+						}
 									
-									//Add the reservation object to the ArrayList where node belongs.
+						//Add the reservation object to the ArrayList where node belongs.
 									
-									if(appState.getOrbitNodes().containsKey(resource_name)){
-										Log.i("JSONParser setLeases", "grid");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getOrbitNodes().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getOrbitNodes().put(resource_name, resourcesData);
-									}
-									else if(appState.getGridNodes().containsKey(resource_name)){
-										Log.i("JSONParser setLeases", "grid");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getGridNodes().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getGridNodes().put(resource_name, resourcesData);
-									}
-									else if(appState.getUsrpNodes().containsKey(resource_name)) {
-										Log.i("JSONParser setLeases", "usrp");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getUsrpNodes().get(resource_name);
-										if(resourcesData == null)
-											Log.i("nodesData","null");
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getUsrpNodes().put(resource_name, resourcesData);
-									}
-									else if(appState.getDisklessNodes().containsKey(resource_name)) {
-										Log.i("JSONParser setLeases", "diskless");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getDisklessNodes().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getDisklessNodes().put(resource_name, resourcesData);
-									}
-									else if(appState.getIcarusNodes().containsKey(resource_name)) {
-										Log.i("JSONParser setLeases", "icarus");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getIcarusNodes().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getIcarusNodes().put(resource_name, resourcesData);
-									}
-									else if(appState.getBaseStations().containsKey(resource_name)){
-										Log.i("JSONParser setLeases", "Base STations");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getBaseStations().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getBaseStations().put(resource_name, resourcesData);
-									}
-									else if(appState.getChannels802_11a().containsKey(resource_name)){
-										Log.i("JSONParser setLeases", "channel 802_11a");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getChannels802_11a().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getChannels802_11a().put(resource_name, resourcesData);
-									}
-									else if(appState.getChannels802_11bg().containsKey(resource_name)){
-										Log.i("JSONParser setLeases", "channel 802_11bg");
-										//Get nodesData which contains uuid value and add the reservation
-										ResourcesData resourcesData = appState.getChannels802_11bg().get(resource_name);
-										resourcesData.setReservations(reservation);
-										//overwrite the entry of the HashMap
-										appState.getChannels802_11bg().put(resource_name, resourcesData);
-									}
-								    else {
-										Log.w("Node name", "unknown");
+						if(appState.getOrbitNodes().containsKey(resource_name)){
+							Log.i("JSONParser setLeases", "grid");
+							//Get nodesData which contains uuid value and add the reservation
+							ResourcesData resourcesData = appState.getOrbitNodes().get(resource_name);
+							resourcesData.setReservations(reservation);
+							//overwrite the entry of the HashMap
+							appState.getOrbitNodes().put(resource_name, resourcesData);
+						} else if (appState.getGridNodes().containsKey(resource_name)){
+							Log.i("JSONParser setLeases", "grid");
+							//Get nodesData which contains uuid value and add the reservation
+							ResourcesData resourcesData = appState.getGridNodes().get(resource_name);
+							resourcesData.setReservations(reservation);
+							//overwrite the entry of the HashMap
+							appState.getGridNodes().put(resource_name, resourcesData);
+						} else if (appState.getUsrpNodes().containsKey(resource_name)) {
+							Log.i("JSONParser setLeases", "usrp");
+							//Get nodesData which contains uuid value and add the reservation
+							ResourcesData resourcesData = appState.getUsrpNodes().get(resource_name);
+							if(resourcesData == null) {
+								Log.i("nodesData","null");
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getUsrpNodes().put(resource_name, resourcesData);
+							} else if (appState.getDisklessNodes().containsKey(resource_name)) {
+								Log.i("JSONParser setLeases", "diskless");
+								//Get nodesData which contains uuid value and add the reservation
+								ResourcesData resourcesData = appState.getDisklessNodes().get(resource_name);
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getDisklessNodes().put(resource_name, resourcesData);
+							} else if (appState.getIcarusNodes().containsKey(resource_name)) {
+								Log.i("JSONParser setLeases", "icarus");
+								//Get nodesData which contains uuid value and add the reservation
+								ResourcesData resourcesData = appState.getIcarusNodes().get(resource_name);
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getIcarusNodes().put(resource_name, resourcesData);
+							} else if (appState.getBaseStations().containsKey(resource_name)){
+								Log.i("JSONParser setLeases", "Base STations");
+								//Get nodesData which contains uuid value and add the reservation
+								ResourcesData resourcesData = appState.getBaseStations().get(resource_name);
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getBaseStations().put(resource_name, resourcesData);
+							} else if (appState.getChannels802_11a().containsKey(resource_name)){
+								Log.i("JSONParser setLeases", "channel 802_11a");
+								//Get nodesData which contains uuid value and add the reservation
+								ResourcesData resourcesData = appState.getChannels802_11a().get(resource_name);
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getChannels802_11a().put(resource_name, resourcesData);
+							} else if(appState.getChannels802_11bg().containsKey(resource_name)){
+								Log.i("JSONParser setLeases", "channel 802_11bg");
+								//Get nodesData which contains uuid value and add the reservation
+								ResourcesData resourcesData = appState.getChannels802_11bg().get(resource_name);
+								resourcesData.setReservations(reservation);
+								//overwrite the entry of the HashMap
+								appState.getChannels802_11bg().put(resource_name, resourcesData);
+							} else {
+								Log.w("Node name", "unknown");
 									
-									}
-								}
-								check_for_resources_names_duplicates.clear();
-						}	   			
-		    	}	
+							}
+						}
+						check_for_resources_names_duplicates.clear();
+					}	   			
+			}	
 		    	
 		    	
-		    	public Reservation setTimeAndDateOnReservation(JSONObject leaseObj) throws JSONException{
-		   	
-		    		
-		    		Log.i("JSONTestbedParser", "set time and date");
+			public Reservation setTimeAndDateOnReservation(JSONObject leaseObj) throws JSONException{
+
+				Log.i("JSONTestbedParser", "set time and date");
 		    		    		
-		    		String valid_from = leaseObj.getString("valid_from");
-					Log.i("valid_from is", valid_from);
+				String valid_from = leaseObj.getString("valid_from");
+				Log.i("valid_from is", valid_from);
 				
-					String valid_until = leaseObj.getString("valid_until");
-					Log.i("valid_until is", valid_until);
-					
-					Reservation reservation = new Reservation(valid_from, valid_until);
+				String valid_until = leaseObj.getString("valid_until");
+				Log.i("valid_until is", valid_until);
+				
+				Reservation reservation = new Reservation(valid_from, valid_until);
 							
-					return reservation;
-		   	 	}
-		    }
-		    
-		    
-		  
-	 		
-	 		
+				return reservation;
+			}
 	}
+		    
+		    
+}
 
