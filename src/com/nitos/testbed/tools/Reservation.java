0f0c2011a35e0ed4f474cc661c087e63a05a91be
zoevas <zoevas1@gmail.com>
2017-04-18 13:07:59 +0300

    Update Reservation.java

--- src/com/nitos/testbed/tools/Reservation.java
+++ src/com/nitos/testbed/tools/Reservation.java
@@ -8,27 +8,24 @@ import android.util.Log;
 
 public class Reservation {
 
-	   DateTime dateTimeFrom;
-	   DateTime dateTimeUntil;
-	   	  
-	   
-	   public Reservation(String valid_from, String valid_until){
-		 
-		    DateTimeZone zoneUTC = DateTimeZone.UTC;
-		    dateTimeFrom = new DateTime(valid_from, zoneUTC);
-	  		Log.w("Reservation", "" + dateTimeFrom.getZone());
-	  		Log.i("Reservation", dateTimeFrom.toString());
-	  		dateTimeUntil = new DateTime(valid_until, zoneUTC);
-	  		Log.i("Reservation", dateTimeUntil.toString());
-	   }
+	DateTime dateTimeFrom;
+	DateTime dateTimeUntil;
+	   	     
+	public Reservation(String valid_from, String valid_until){
+		DateTimeZone zoneUTC = DateTimeZone.UTC;
+		dateTimeFrom = new DateTime(valid_from, zoneUTC);
+		Log.w("Reservation", "" + dateTimeFrom.getZone());
+		Log.i("Reservation", dateTimeFrom.toString());
+		dateTimeUntil = new DateTime(valid_until, zoneUTC);
+		Log.i("Reservation", dateTimeUntil.toString());
+	}
 	  
 	   
-	   public DateTime getDateTimeFrom(){
-		   return dateTimeFrom;
-		   
-	   }
+	public DateTime getDateTimeFrom(){
+		return dateTimeFrom;
+	}
 	   
-	   public DateTime getDateTimeUntil(){
-		   return dateTimeUntil;
-	   }
+	public DateTime getDateTimeUntil(){
+		return dateTimeUntil;
+	}
 }
