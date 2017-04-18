8aca3de8690e8581ff0068fcf72ca22652f7322e
zoevas <zoevas1@gmail.com>
2017-04-18 09:56:32 +0300

    Update NitosSchedulerActivity.java

--- src/com/nitos/testbed/tools/NitosSchedulerActivity.java
+++ src/com/nitos/testbed/tools/NitosSchedulerActivity.java
@@ -19,48 +19,43 @@ public class NitosSchedulerActivity extends MainMenuActivity  {
 		
 		
 		ActionBar  actionBar = getSupportActionBar(); 
-       /* ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#172741"));     
-        actionBar.setBackgroundDrawable(colorDrawable);*/
+		/* ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#172741"));     
+  		actionBar.setBackgroundDrawable(colorDrawable);*/
         
-        actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+               
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         
-    
+		ActionBar.Tab selectTab = actionBar.newTab().setText(getString(R.string.tabname_selection_menu));
+  		ActionBar.Tab outdoorTestbedTab = actionBar.newTab().setText(getString(R.string.tabname_outdoor_testbed));
+		ActionBar.Tab indoorTestbedTab = actionBar.newTab().setText(getString(R.string.tabname_indoor_testbed));
         
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		Fragment selectFragment = new SchedulerChooserFragment();
+		Fragment outdoorTestbedFragment = new OutdoorTestbedFragment();
+		Fragment indoorTestbedFragment = new IndoorTestbedFragment();   
         
-        ActionBar.Tab selectTab = actionBar.newTab().setText(getString(R.string.tabname_selection_menu));
-        ActionBar.Tab outdoorTestbedTab = actionBar.newTab().setText(getString(R.string.tabname_outdoor_testbed));
-        ActionBar.Tab indoorTestbedTab = actionBar.newTab().setText(getString(R.string.tabname_indoor_testbed));
-        
-        Fragment selectFragment = new SchedulerChooserFragment();
-        Fragment outdoorTestbedFragment = new OutdoorTestbedFragment();
-        Fragment indoorTestbedFragment = new IndoorTestbedFragment();   
-        
-        selectTab.setTabListener(new MyTabsListener(selectFragment, getApplicationContext()));
-        outdoorTestbedTab.setTabListener(new MyTabsListener(outdoorTestbedFragment, getApplicationContext()));
-        indoorTestbedTab.setTabListener(new MyTabsListener(indoorTestbedFragment, getApplicationContext()));
+		selectTab.setTabListener(new MyTabsListener(selectFragment, getApplicationContext()));
+		outdoorTestbedTab.setTabListener(new MyTabsListener(outdoorTestbedFragment, getApplicationContext()));
+		indoorTestbedTab.setTabListener(new MyTabsListener(indoorTestbedFragment, getApplicationContext()));
  
         
-        actionBar.addTab(selectTab);
-        actionBar.addTab(outdoorTestbedTab);
-        actionBar.addTab(indoorTestbedTab);
-        
-        
+		actionBar.addTab(selectTab);
+		actionBar.addTab(outdoorTestbedTab);
+ 		actionBar.addTab(indoorTestbedTab);
 	}
 	 
  
 	
 
 	class MyTabsListener implements ActionBar.TabListener {
-
-		 public Fragment fragment;
-         public Context context;
+		public Fragment fragment;
+		public Context context;
 		
-		 public MyTabsListener(Fragment fragment, Context context) {
-              this.fragment = fragment;
-              this.context = context;
-
-        }
+		public MyTabsListener(Fragment fragment, Context context) {
+			this.fragment = fragment;
+			this.context = context;
+		}
+		
 		@Override
 		public void onTabReselected(Tab arg0, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
@@ -71,15 +66,13 @@ public class NitosSchedulerActivity extends MainMenuActivity  {
 		public void onTabSelected(Tab arg0, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
 			ft.replace(R.id.layoutToReplace_nitosScheduler, fragment);
-			
 		}
 
 		@Override
 		public void onTabUnselected(Tab arg0, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
 			ft.remove(fragment);
-		}
-		
+		}		
 	}
 	 
 }
