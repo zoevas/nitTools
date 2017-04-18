a9b3e6c5cfb012e76fe42ff4b5dba7407f82626e
zoevas <zoevas1@gmail.com>
2017-04-18 21:26:39 +0300

    Update DatePickerFragment.java

--- src/com/nitos/testbed/tools/DatePickerFragment.java
+++ src/com/nitos/testbed/tools/DatePickerFragment.java
@@ -19,35 +19,31 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 	GlobalData appState;
 	
 	
-	 public DatePickerFragment(TextView txtDate) {
+	public DatePickerFragment(TextView txtDate) {
 		super();
 		this.txtDate = txtDate;
 	}
 
 	@Override
-	    public Dialog onCreateDialog(Bundle savedInstanceState) {
-	        // Use the current date as the default date in the picker
-	        DateTime dt = new DateTime();
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current date as the default date in the picker
+		DateTime dt = new DateTime();
 	      
-	        int year = dt.getYear();
-	        int month = dt.getMonthOfYear()-1;
-	        int day = dt.getDayOfMonth();
+		int year = dt.getYear();
+		int month = dt.getMonthOfYear()-1;
+		int day = dt.getDayOfMonth();
 	        
-	        appState = ((GlobalData)getActivity().getApplicationContext()); 
-	        appState.setDateUserFrom(year, month, day);
+		appState = ((GlobalData)getActivity().getApplicationContext()); 
+		appState.setDateUserFrom(year, month, day);
 
-	        // Create a new instance of DatePickerDialog and return it
-	        return new DatePickerDialog(getActivity(), this, year, month, day);
-	    }
+		// Create a new instance of DatePickerDialog and return it
+		return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
 
-	    public void onDateSet(DatePicker view, int year, int month, int day) {
-	    	appState.setDateUserFrom(year, month + 1 , day);
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		appState.setDateUserFrom(year, month + 1 , day);
 
-	    	
-	    	txtDate.setText(new StringBuilder().append(year)
+		txtDate.setText(new StringBuilder().append(year)
 	 			   .append("-").append(String.format("%02d", month + 1) ).append("-").append(String.format("%02d", day)));
-	    }
-	
-	
-	
+	}	
 }
